ebb58f658c8a3de008ccec798753a6e1b4f90390
Constantine <tmail99@gmail.com>
2025-05-06 17:09:04 +0300

    06.05.25 Разрабатывал отдельно, может сильно отлтчаться от предыдущего коммита. Рабочий! Основной прошел автотесты, включая iter2. Теперь значение этого репозитория историческое

--- main.go
+++ main.go
@@ -4,16 +4,15 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"math/rand"
 	"net/http"
-	"regexp"
 	"strings"
-	"time"
+
+	uuid "github.com/satori/go.uuid"
 )
 
 type Storage interface {
-	InsertURL(uid string, url string) error
-	GetURL(uid string) (string, error)
+	Insert(uid string, url string) error
+	Get(uid string) (string, error)
 }
 
 // тип urlStorage и его параметр Data
@@ -28,14 +27,14 @@ func NewStorageStruct() *UrlStorage {
 	}
 }
 
-// тип urlStorage и его метод InsertURL
-func (s *UrlStorage) InsertURL(uid string, url string) error {
+// тип urlStorage и его метод Insert
+func (s *UrlStorage) Insert(uid string, url string) error {
 	s.Data[uid] = url
 	return nil
 }
 
-// тип urlStorage и его метод GetURL
-func (s *UrlStorage) GetURL(uid string) (string, error) {
+// тип urlStorage и его метод Get
+func (s *UrlStorage) Get(uid string) (string, error) {
 	e, existss := s.Data[uid]
 	if !existss {
 		return uid, errors.New("URL with such id doesn`t exist")
@@ -43,68 +42,43 @@ func (s *UrlStorage) GetURL(uid string) (string, error) {
 	return e, nil
 }
 
-// *** Реализую интерфейс Storage********************************
-// создаю запись в объекте типа UrlStorage
-func MakeEntry(s Storage, id string, url string) {
-	s.InsertURL(id, url)
-}
-
-// получаю запись из объекта хранилища
-func GetEntry(s Storage, id string) (string, error) {
-	e, err := s.GetURL(id)
-	return e, err
-}
-
-//***********************************************************
-
+// Функция для генерации сокращённого URL
 func generateShortURL(urlList *UrlStorage, longURL string) string {
-	rand.Seed(time.Now().UnixNano()) // Инициализация генератора случайных чисел
-	runes := []rune(longURL)
-	rand.Shuffle(len(runes), func(i, j int) {
-		runes[i], runes[j] = runes[j], runes[i]
-	})
-	//удаляю из полученной строки все кроме букв и цифр
-	reg := regexp.MustCompile(`[^a-zA-Zа-яА-Я0-9]`)
-	//[:11] здесь мы еще сокращаем строку
-	id := reg.ReplaceAllString(string(runes[:11]), "")
+	// Генерируем уникальный идентификатор (uid) при помощи пакета go.uuid
+	uuidObj := uuid.NamespaceURL
+	uuidStr := uuidObj.String()
+	uuidStr = strings.ReplaceAll(uuidStr, "-", "")
+	uid := uuidStr[:8]
 
-	//Реализуем интерфейс Storage, что в последующем даст возможность
-	//использовать его методы и другим типам
-	MakeEntry(urlList, id, longURL)
+	//Вот здесь создаем запись в нашем объекте (типа *UrlStorage)
+	//map[string]string ["6ba7b811": "https://practicum.yandex.ru/", ]
+	urlList.Insert(uid, longURL)
 
-	return "/" + id
+	return "/" + uid
 }
 
 // тип urlStorage и его метод PostHandler
-func (ts *UrlStorage) PostHandler(w http.ResponseWriter, req *http.Request) {
-	switch req.Method {
-	case http.MethodPost:
-		switch req.Header.Get("Content-Type") {
-		case "text/plain":
-			//param- тело запроса (тип []byte)
-			param, err := io.ReadAll(req.Body)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusBadRequest)
-				return
-			}
-
-			// Генерируем сокращённый URL и создаем запись в нашем хранилище
-			shortURL := req.Host + generateShortURL(ts, string(param))
-
-			w.WriteHeader(http.StatusCreated)
-			fmt.Fprint(w, shortURL)
-		default:
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprint(w, "Content-Type isn`t text/plain")
-		}
-	default:
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, "Method not allowed")
+func (s *UrlStorage) PostHandler(w http.ResponseWriter, req *http.Request) {
+	param, err := io.ReadAll(req.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
+
+	// Преобразуем тело запроса (тип []byte) в строку:
+	longURL := string(param)
+	// Генерируем сокращённый URL и создаем запись в нашем хранилище
+	shortURL := "http://" + req.Host + generateShortURL(s, longURL)
+
+	// Устанавливаем статус ответа 201
+	w.WriteHeader(http.StatusCreated)
+
+	fmt.Fprint(w, shortURL)
+
 }
 
 // тип urlStorage и его метод GetHandler
-func (ts *UrlStorage) GetHandler(w http.ResponseWriter, req *http.Request) {
+func (s *UrlStorage) GetHandler(w http.ResponseWriter, req *http.Request) {
 	//Тесты подсказали добавить проверку на метод:
 	switch req.Method {
 	case http.MethodGet:
@@ -115,11 +89,7 @@ func (ts *UrlStorage) GetHandler(w http.ResponseWriter, req *http.Request) {
 		// Но получаем лишний "/"
 		id := strings.TrimPrefix(req.RequestURI, "/")
 
-		// //Так не реализуя интерфейс
-		//longURL, err := ts.GetURL(id)
-
-		//Так реализуя интерфейс
-		longURL, err := GetEntry(ts, id)
+		longURL, err := s.Get(id)
 		if err != nil {
 			//http.Error(w, "URL not found", http.StatusBadRequest)
 			w.Header().Set("Location", err.Error())
@@ -133,67 +103,67 @@ func (ts *UrlStorage) GetHandler(w http.ResponseWriter, req *http.Request) {
 		w.WriteHeader(http.StatusTemporaryRedirect)
 	default:
 		w.Header().Set("Location", "Method not allowed")
-		w.WriteHeader(http.StatusBadRequest)
+		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
 }
-
-// *********************************************************************************
-// Секция сервера
-// Здесь переопределен стандартный ServeMux маршрутизатором CustomMux,
-// Цель- возвращать 400 вместо 405
-type CustomMux struct {
-	*http.ServeMux
-}
-
-func (m *CustomMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// Проверяем, есть ли такой путь
-	_, pattern := m.Handler(r)
-	if pattern == "" {
-		// // Если эндпоинта нет вообще — 404
-		// http.NotFound(w, r)
-
-		// Но мне нужно 400
-		http.Error(w, "Bad Request", http.StatusBadRequest)
-		return
-	}
-
-	// Если эндпоинт есть, но метод не совпадает — 400
-	if !m.isMethodAllowed(r) {
-		http.Error(w, "Bad Request", http.StatusBadRequest)
-		return
-	}
-
-	// Иначе передаем обработку стандартному ServeMux
-	m.ServeMux.ServeHTTP(w, r)
-}
-
-// isMethodAllowed проверяет, разрешен ли метод для данного пути
-func (m *CustomMux) isMethodAllowed(r *http.Request) bool {
-	// Получаем зарегистрированный обработчик для этого пути
-	handler, _ := m.Handler(r)
-
-	// Если обработчик — это ServeMux (значит, метод не совпадает)
-	_, isServeMux := handler.(*http.ServeMux)
-	return !isServeMux
-}
-
-// setupServer создает и настраивает HTTP-сервер
-func setupServer() *CustomMux {
-	mux := &CustomMux{http.NewServeMux()}
-
-	//создаю объект типа UrlStorage
-	storageInstance := NewStorageStruct()
-
-	//обращаюсь к методам UrlStorage
-	mux.HandleFunc("POST /{$}", storageInstance.PostHandler)
-	mux.HandleFunc("GET /{id}", storageInstance.GetHandler)
-
-	return mux
-}
-
-//*************************************************************************************
 
 func main() {
-	server := setupServer()
-	http.ListenAndServe(":8080", server)
+	// Любая функция удовлетворяющая интерфейсу Handler будет реализовывать его.
+	//
+	//	Handler interface {
+	//	   ServeHTTP(ResponseWriter, *Request)}
+	//
+	// Как я понимаю выбор для встроенных данных типа interface имеет смыслом возможность
+	// создания бесконечности собственных функций удовлетворяющих ему.
+	//
+	// You could not pass it as a handler just yet. After all, it’s just a function and does not provide that ServeHTTP method signature.
+	// You can use HandlerFunc to wrap this function, and have it satisfy the interface!
+	// https://gopherdojo.com/handler-vs-handlerfunc/
+	//
+
+	// создаю маршрутизатор
+	mux := http.NewServeMux()
+	// создаю объект типа UrlStorage
+	storage := NewStorageStruct()
+	//
+
+	// Не путать две сигнатуры в пакете http - HandleFunc и HandlerFunc (!!!)
+	//
+	// HandleFunc является методом типа ServeMux
+	// func (mux *http.ServeMux) HandleFunc(pattern string, handler func(http.ResponseWriter, *http.Request))
+	// or
+	// http.HandleFunc(pattern string, handler func(ResponseWriter, *Request))
+	//
+	// Метод HandleFunc регистрирует функцию-обработчик для заданного шаблона.
+	// Если данный шаблон конфликтует с уже зарегистрированным, HandleFunc в панике.
+	//
+	// HandlerFunc является типом
+	// type HandlerFunc func(ResponseWriter, *Request)
+	// Тип HandlerFunc — это адаптер (обертка), позволяющий использовать обычные функции (но реализующие Handler interface)
+	// в качестве обработчиков [Handler] HTTP.
+	// Если f — это функция с соответствующей сигнатурой, HandlerFunc(f) — это [Handler], который вызывает f.
+	// The HandlerFunc type is an adapter to allow the use of ordinary functions as HTTP handlers.
+	// If f is a function with the appropriate signature, HandlerFunc(f) is a [Handler] that calls f.
+	//
+	// А вот ServeHTTP это метод типа HandlerFunc
+	// он и вызывает нашу обычную функцию (в примере выше f)
+	// ServeHTTP calls f(w, r).
+	//
+	// func (f http.HandlerFunc) ServeHTTP(w http.ResponseWriter, r *http.Request)
+
+	// обращаюсь к методам UrlStorage
+	// но,
+	// через метод HandlerFunc для обертывания удовлетворяющей интерфейсу Handler
+	// func (ts *UrlStorage) PostHandler(w http.ResponseWriter, req *http.Request) {}
+	// (она реализует его методы- ResponseWriter и Request)
+	// Без этого ее нельзя передать как обработчик т.к. она не предоставляет сигнатуру метода ServeHTTP.
+	mux.HandleFunc("POST /{$}", storage.PostHandler)
+	mux.HandleFunc("GET /{id}", storage.GetHandler)
+	//
+	http.HandleFunc("POST /{$}", storage.PostHandler)
+	//func http.HandleFunc(pattern string, handler func(http.ResponseWriter, *http.Request))
+	// http.HandlerFunc
+	// //type HandlerFunc func(ResponseWriter, *Request)
+
+	http.ListenAndServe("localhost:8080", mux)
 }
